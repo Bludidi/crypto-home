8cbd91b9d36f6121daded33f533b3f0048907386
Bludidi <bludidi5@gmail.com>
2022-12-08 14:46:17 +0200

    fix linter errors

--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
-import NavBar from '../components/NavBar'
+import NavBar from '../components/NavBar';
 
 test('renders the navbar component', () => {
   const { asFragment } = render(
