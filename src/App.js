75359bdc512a39a1222cf04fba2e50e67c9cd4be
Bludidi <bludidi5@gmail.com>
2023-06-28 17:20:08 +0200

    Add meta data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Helmet } from 'react-helmet';
 import Cryptos from './components/Cryptos';
 import Crypto from './components/Crypto';
 import NavBar from './components/NavBar';
@@ -12,6 +13,17 @@ function App() {
         path="/"
         element={(
           <>
+            <Helmet>
+              <title>Crypto Home</title>
+              <meta name="description" content="Crypto Home is a web app that shows top 10 crypto currencies ordered by their value" />
+              <meta property="og:title" content="Crypto Home" />
+              <meta
+                property="og:description"
+                content="Crypto Home is a web app that shows top 10 crypto currencies ordered by their value"
+              />
+              <meta property="og:image" content="./src/assets/crypto_home.png" />
+              <meta property="og:url" content="https://hilarious-pasca-2d43e9.netlify.app/" />
+            </Helmet>
             <NavBar />
             <Cryptos />
           </>
